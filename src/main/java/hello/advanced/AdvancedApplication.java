a35d12b522ed5e268c39e4eb2dce1bbca1e75af4
sunghongmo <hm.sung@softwareinlife.com>
2022-01-16 08:20:31 +0900

    예제 프로젝트 만들기 v2

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -1,11 +1,13 @@
 package hello.advanced;
 
 import hello.advanced.config.AppV1Config;
+import hello.advanced.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+@Import(AppV2Config.class)
+//@Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.app")
 public class AdvancedApplication {
 
---
+++ src/main/java/hello/advanced/config/AppV2Config.java
@@ -0,0 +1,26 @@
+package hello.advanced.config;
+
+import hello.advanced.app.v2.OrderControllerV2;
+import hello.advanced.app.v2.OrderRepositoryV2;
+import hello.advanced.app.v2.OrderServiceV2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV2Config {
+
+    @Bean
+    public OrderControllerV2 orderControllerV1() {
+        return new OrderControllerV2(orderServiceV2());
+    }
+
+    @Bean
+    public OrderServiceV2 orderServiceV2() {
+        return new OrderServiceV2(orderRepositoryV2());
+    }
+
+    @Bean
+    public OrderRepositoryV2 orderRepositoryV2() {
+        return new OrderRepositoryV2();
+    }
+}
