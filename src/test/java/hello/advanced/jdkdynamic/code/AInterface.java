c70ab3468ea9e63ab8fc29e16853f894c9277571
sunghongmo <hm.sung@softwareinlife.com>
2022-01-16 14:01:41 +0900

    JDK 동적 프록시 - 소개

---
+++ src/test/java/hello/advanced/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package hello.advanced.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/hello/advanced/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package hello.advanced.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
