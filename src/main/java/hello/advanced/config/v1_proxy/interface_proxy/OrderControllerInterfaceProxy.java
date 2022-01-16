a7e73ff64601331867229c565d74b84c26b91ef2
sunghongmo <hm.sung@softwareinlife.com>
2022-01-16 10:25:33 +0900

    인터페이스 기반 프록시 - 적용

---
+++ src/main/java/hello/advanced/config/v1_proxy/interface_proxy/OrderControllerInterfaceProxy.java
@@ -0,0 +1,33 @@
+package hello.advanced.config.v1_proxy.interface_proxy;
+
+import hello.advanced.app.v1.OrderControllerV1;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderControllerInterfaceProxy implements OrderControllerV1 {
+
+    private final OrderControllerV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderController.request()");
+            //target 호출
+            String result = target.request(itemId);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+    @Override
+    public String noLong() {
+        return target.noLong();
+    }
+}
---
+++ src/main/java/hello/advanced/config/v1_proxy/interface_proxy/OrderServiceInterfaceProxy.java
@@ -0,0 +1,26 @@
+package hello.advanced.config.v1_proxy.interface_proxy;
+
+import hello.advanced.app.v1.OrderServiceV1;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderServiceInterfaceProxy implements OrderServiceV1 {
+
+    private final OrderServiceV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.orderItem()");
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
