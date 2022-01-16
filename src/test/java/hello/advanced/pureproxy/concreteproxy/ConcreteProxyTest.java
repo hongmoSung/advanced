f182be4eac7d28d7ec8f1f0fb1939f1b6224a719
sunghongmo <hm.sung@softwareinlife.com>
2022-01-16 11:10:47 +0900

    구체 클래스 기반 프록시 - 예제2

--- src/test/java/hello/advanced/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/advanced/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.advanced.pureproxy.concreteproxy;
 
 import hello.advanced.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.advanced.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.advanced.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
     void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 }
