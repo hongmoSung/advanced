80491153460e77f94a3e613e95720b1019b6bacb
sunghongmo <hm.sung@softwareinlife.com>
2022-01-18 19:49:59 +0900

    예제 코드1 - 어드바이저

---
+++ src/test/java/hello/advanced/advisor/AdvisorTest.java
@@ -0,0 +1,26 @@
+package hello.advanced.advisor;
+
+import hello.advanced.common.advice.TimeAdvice;
+import hello.advanced.common.service.ServiceImpl;
+import hello.advanced.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        proxy.save();
+        proxy.find();
+    }
+
+}
