c261724ce4f11ec92b617934d7824ced4cf4794a
sunghongmo <hm.sung@softwareinlife.com>
2022-01-18 21:51:52 +0900

    빈 후처리기 - 적용

---
+++ src/main/java/hello/advanced/config/v4_postprocessor/BeanPostProcessorConfig.java
@@ -0,0 +1,36 @@
+package hello.advanced.config.v4_postprocessor;
+
+import hello.advanced.config.AppV1Config;
+import hello.advanced.config.AppV2Config;
+import hello.advanced.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.advanced.config.v4_postprocessor.postprocessor.PackageLogTraceProxyPostProcessor;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class BeanPostProcessorConfig {
+
+    @Bean
+    public PackageLogTraceProxyPostProcessor
+    logTraceProxyPostProcessor(LogTrace logTrace) {
+        return new PackageLogTraceProxyPostProcessor("hello.advanced.app", getAdvisor(logTrace));
+    }
+
+    private Advisor getAdvisor(LogTrace logTrace) {
+        //pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        //advisor = pointcut + advice
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
